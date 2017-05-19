8bbc9a5953f766faba65c3ad97f49026cb05ed70
Leon.yan <yanyin1986@gmail.com>
2017-05-20 08:06:50 +0900

    add gitignore

---
+++ mNoisi/MNSoundTrackTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MNSoundTrackTableViewCell.swift
+//  mNoisi
+//
+//  Created by Leon.yan on 18/05/2017.
+//  Copyright © 2017 Leon.yan. All rights reserved.
+//
+
+import UIKit
+
+class MNSoundTrackTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
